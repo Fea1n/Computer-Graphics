02c66d50ee274d6f13990cdf1b88abe42196e91f
Fea1n <60054083+Fea1n@users.noreply.github.com>
2022-04-26 23:05:53 +0800

    加入后处理效果

--- 02_Project/Web_Engine/demo00/src/views/Etc.vue
+++ 02_Project/Web_Engine/demo00/src/views/Etc.vue
@@ -9,6 +9,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { BloomEffect, EffectComposer, EffectPass, RenderPass } from "postprocessing"
 
 export default {
   data() {
@@ -17,8 +18,10 @@ export default {
       mesh: null,
       camera: null,
       scene: null,
+      spotLight:null,
       renderer: null,
-      controls: null
+      controls: null,
+      composer:null
     }
   },
   mounted() {
@@ -33,70 +36,115 @@ export default {
       this.createCamera() // 创建相机
       this.createRender() // 创建渲染器
       this.createControls() // 创建控件对象
+      this.createComposer() //创建后处理对象
       this.render() // 渲染
     },
     // 创建场景
     createScene() {
       this.scene = new THREE.Scene()
+      this.scene.background = new THREE.Color(0x000000)
     },
     // 加载GLTF模型
     loadGLTF() {
       const THIS = this
       const loader = new GLTFLoader()
-      loader.load(`${THIS.publicPath}static/models/lisa/scene.gltf`, model=> {
-        model.scene.children[0].scale.set(50, 50, 50)
-        this.scene.add(model.scene.children[0])
-        console.log(model)
+      loader.load(`${THIS.publicPath}static/models/little_chestnut/scene.gltf`, result=> {
+        // model.scene.children[0].scale.set(20, 20, 20)
+        const model = result.scene.children[0]
+        model.traverse((n)=>
+          {
+            if(n.isMesh){
+            n.castShadow = true;
+            n.receiveShadow = true
+            //提高纹理的各向异性
+            if (n.material.map) n.material.map.anisotropy =100
+            }
+          }
+        )
+        this.scene.add(model)
+        this.render()
+        // console.log(model)
       })
-      
     },
-
     // 创建光源
     createLight() {
       // 环境光
-      const ambientLight = new THREE.AmbientLight(0xffffff, 0.1) // 创建环境光
-      this.scene.add(ambientLight) // 将环境光添加到场景
-
-      const spotLight = new THREE.SpotLight(0xffffff) // 创建聚光灯
-      spotLight.position.set(150, 150, 150)
-      spotLight.castShadow = true
-      this.scene.add(spotLight)
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.1) 
+      this.scene.add(ambientLight)
+      // 半球光
+      const hemisphereLight = new THREE.HemisphereLight(0xffff55, 0x00ffff, 0.6)
+      this.scene.add(hemisphereLight)
+      // 聚光灯(由于光线追踪需求，暂时设定为动态全局变量)
+      this.spotLight = new THREE.SpotLight(0xffa95c,4) 
+      // spotLight.position.set(150, 150, 150)
+      this.spotLight.castShadow = true
+      this.spotLight.shadow.bias = -0.0001
+      this.spotLight.shadow.mapSize.width = 10000
+      this.spotLight.shadow.mapSize.height = 10000
+      this.scene.add(this.spotLight)
     },
     // 创建相机
     createCamera() {
       const element = document.getElementById('container')
-      const width = element.clientWidth // 窗口宽度
-      const height = element.clientHeight // 窗口高度
-      const k = width / height // 窗口宽高比
+      const width = element.clientWidth 
+      const height = element.clientHeight 
+      const k = width / height 
       // PerspectiveCamera( fov, aspect, near, far )
       this.camera = new THREE.PerspectiveCamera(35, k, 0.1, 1000)
-      this.camera.position.set(150, 150, 150) // 设置相机位置
+      this.camera.position.set(0, 3, 5) // 设置相机位置
 
-      this.camera.lookAt(new THREE.Vector3(10, 40, 0)) // 设置相机方向
+      this.camera.lookAt(new THREE.Vector3(10, 40, 0)) 
       this.scene.add(this.camera)
     },
     // 创建渲染器
     createRender() {
       const element = document.getElementById('container')
       this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
-      this.renderer.setSize(element.clientWidth, element.clientHeight) // 设置渲染区域尺寸
-      this.renderer.shadowMap.enabled = true // 显示阴影
+      
+      // 设置渲染区域尺寸
+      this.renderer.setSize(element.clientWidth, element.clientHeight) 
+      //阴影映射
+      this.renderer.shadowMap.enabled = true 
       this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
-      this.renderer.setClearColor(0x3f3f3f, 1) // 设置背景颜色
+      //色调映射
+      this.renderer.toneMapping = THREE.ReinhardToneMapping
+      this.renderer.toneMappingExposure = 3
       element.appendChild(this.renderer.domElement)
-    },
-
-    render() {
-      if (this.mesh) {
-        this.mesh.rotation.z += 0.006
-      }
-      this.renderer.render(this.scene, this.camera)
-      requestAnimationFrame(this.render)
     },
     // 创建控件对象
     createControls() {
       this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    }
+    },
+    // 创建动画函数
+    // createAnimate(){
+
+    // },
+    //创建后处理函数
+    createComposer(){
+        this.composer = new EffectComposer(this.renderer)
+        this.composer.addPass(new RenderPass(this.scene,this.camera))
+        
+        //辉光效果
+        const effectPass = new EffectPass(this.camera,new BloomEffect({
+          intensity:3,
+          luminanceThreshold:0.8,
+          width:100,
+          height:100,
+        }))
+
+        // this.composer.addPass(effectPass)
+    },
+    // 渲染
+    render() {
+      requestAnimationFrame(this.render)
+      this.spotLight.position.set(
+        this.camera.position.x +5,
+        this.camera.position.y +5,
+        this.camera.position.z -5
+      )
+      this.composer.render(this.scene, this.camera)
+      // this.render(this.scene,this.camera)
+    },
   }
 }
 </script>
@@ -104,6 +152,6 @@ export default {
 
 <style>
 #container {
-    height: 400px;
+    height: 500px;
 }
 </style>
--- 02_Project/Web_Engine/demo00/src/views/Jsloader.vue
+++ 02_Project/Web_Engine/demo00/src/views/Jsloader.vue
@@ -1,14 +1,23 @@
 <template>
     <div id="jsload">
-            <div id="container">
-                
-            </div>
+        <div id="container">
+        </div>
     </div>
 </template>
+<!-- 
 <script>
+
+import * as THREE from 'three';
+
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
+
+
 
 </script>
 
 <style>
-
-</style>
+#container {
+    height: 500px;
+}
+</style> -->
